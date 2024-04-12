ba7b9176f6de9ffbd317a42405534d29a38e3ebe
James Shapiro <jamesshapiro@gmail.com>
2024-04-12 18:39:46 -0400

    add changes to next-personal website

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,6 @@ import Navigation from '@/components/Navigation';
 export default function Home() {
   return (
     <>
-      {/* <Navigation /> */}
-      {/* <main className={styles.main}>
-        <p>Shalom World!</p>
-      </main> */}
     </>
   );
 }
